40f262fa71967343a3b1b1e2a4addb5d9c026be0
108nitish <166498889+108nitish@users.noreply.github.com>
2025-04-30 19:32:55 +0530

    Add files via upload

---
+++ Backend/controller/predictionController.js
@@ -0,0 +1,52 @@
+import { validationResult } from "express-validator";
+import fetch from "node-fetch"
+// import doctor_vs_disease from "../disease_data/doctor_vs_disease.json" assert { type: "json" };
+import Doctor from "../models/doctorModel.js";
+// import diseaseInfo from "../disease_data/disease_info.json" assert { type: "json" };
+
+import fs from "fs";
+
+const doctor_vs_disease = JSON.parse(fs.readFileSync(new URL("../disease_data/doctor_vs_disease.json", import.meta.url)));
+const diseaseInfo = JSON.parse(fs.readFileSync(new URL("../disease_data/disease_info.json", import.meta.url)));
+
+
+export const predictDisease = async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  const features = req.body;
+
+  try {
+    const response = await fetch("http://127.0.0.1:5001/predictdisease", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(features),
+    });
+
+    const data = await response.json();
+    const disease = data.predicted_disease
+
+    const doctor = doctor_vs_disease[disease]
+
+    let enrolledDoctor = await Doctor.findOne({ specialization: doctor });
+
+    const disease_info = diseaseInfo[disease]
+
+    if(!doctor){
+        enrolledDoctor = []
+    }else{
+      enrolledDoctor = [enrolledDoctor]; 
+    }
+
+    res.status(200).json({ disease, doctor, enrolledDoctor, disease_info });
+    console.log("Disease", disease);
+    console.log("Doctor", doctor);
+    console.log("Enrolled Doctor", enrolledDoctor);
+    console.log("Disease Info", disease_info);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Internal Server Error in predictionController");
+  }
+};
---
+++ Backend/server.js
@@ -0,0 +1,44 @@
+import express from "express";
+import connectToMongoDB from "./database/db.js";
+import cors from "cors";
+import authRoutes from "./routes/authRoutes.js";
+import doctorRoutes from "./routes/doctorRoutes.js";
+import predictionRoutes from "./routes/predictionRoutes.js";
+import { fileURLToPath } from "url";
+import path from "path";
+import cookieParser from "cookie-parser";
+
+const PORT = 6000;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+app.use(express.json({ limit: "100mb" }));
+app.use(cookieParser());
+app.use(cors({
+  origin: 'http://localhost:5173',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true,
+  optionsSuccessStatus: 204
+}));
+
+app.use(express.urlencoded({ extended: false }));
+
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static("/root/MedAssist/dist"));
+
+app.use("/api/auth", authRoutes);
+app.use("/api/doctor", doctorRoutes);
+app.use("/api/prediction", predictionRoutes);
+
+const frontendBuildPath = path.join(__dirname, "../frontend/dist");
+app.use(express.static(frontendBuildPath));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(frontendBuildPath, "index.html"));
+});
+
+app.listen(PORT, () => {
+  connectToMongoDB();
+  console.log(`Server Running on http://localhost:${PORT}`);
+});
