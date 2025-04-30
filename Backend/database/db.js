1d84e25b8e7c2a47fea3c1e6473fa370fa40317e
108nitish <166498889+108nitish@users.noreply.github.com>
2025-04-30 19:34:28 +0530

    Update db.js

--- Backend/database/db.js
+++ Backend/database/db.js
@@ -1,1 +1,13 @@
+import mongoose from "mongoose";
+// console.log("MONGODB_URI from env:", process.env.MONGODB_URI);
+const MONGODB_URI = "YOUR_URI"
+const connectToMongoDB = async () => {
+	try {
+		await mongoose.connect(MONGODB_URI);
+		console.log("Connected to MongoDB");
+	} catch (error) {
+		console.log("Error connecting to MongoDB", error.message);
+	}
+};
 
+export default connectToMongoDB;
